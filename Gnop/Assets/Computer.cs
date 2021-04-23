526510944d12ecee78c983f9bad79e0cffdef293
parzival-3x <bg193b@gmail.com>
2021-04-23 12:46:42 -0500

    Update Computer.cs
    basic variables before class ends

--- Gnop/Assets/Computer.cs
+++ Gnop/Assets/Computer.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Computer : MonoBehaviour
 {
+    public float moveSpeed = 30f;
+    public float topBounds = 17.85f;
+    public float bottomBounds = -14.24f;
+    public Vector2 starttingPosition = new Vector2(21.61402f, 1.135658f);
+    private GameObject ball;
+    private Vector2 ballPos;
+
     // Start is called before the first frame update
     void Start()
     {
