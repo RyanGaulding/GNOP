9c9aee2f32b4aebc7865420f06f1b9fc43c42aed
nsx00 <mgrav@tutanota.com>
2021-04-23 12:08:09 -0500

    Paddles move seperatley
    changed project settings to make 2 player possible

--- Gnop/Assets/MoveRacket.cs
+++ Gnop/Assets/MoveRacket.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class MoveRacket : MonoBehaviour
 {
     public float speed = 30;
+    public string axis = "Vertical";
 
     // Start is called before the first frame update
     void FixedUpdate()
     {
-        float v = Input.GetAxisRaw("Vertical");
+        float v = Input.GetAxisRaw(axis);
         GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
     }
 }
