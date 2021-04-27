9f518b36c30518e8f346d8ffac81f9dac0e6d0dd
Ryan Gaulding <S622501@cfisd.loc>
2021-04-27 12:44:28 -0500

    Increases Ball Size when hit

---
+++ Gnop/Assets/IncreaseSizeBall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseSizeBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject temp = collision.gameObject;
+        temp.transform.localScale = new Vector3(4, 4, 4);
+       
+       
+        
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
