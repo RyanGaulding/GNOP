bfa7847a9b8983842a87ebad0835925e301897b3
nsx00 <mgrav@tutanota.com>
2021-04-29 12:58:15 -0500

    Update PointLeft.cs

--- Gnop/Assets/PointLeft.cs
+++ Gnop/Assets/PointLeft.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PointLeft : MonoBehaviour
 {
     private int scoreLeft;
+    public TextMeshProUGUI countText;
     // Start is called before the first frame update
     void Start()
     {
