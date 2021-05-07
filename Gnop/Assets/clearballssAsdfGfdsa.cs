b6d51554c090e73faf9bc3ea87be9651acca119d
Ryan Gaulding <S622501@cfisd.loc>
2021-05-07 12:01:51 -0500

    Stuff

--- Gnop/Assets/clearballssAsdfGfdsa.cs
+++ Gnop/Assets/clearballssAsdfGfdsa.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpazAttack : MonoBehaviour
+public class clearballssAsdfGfdsa : MonoBehaviour
 {
+    // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       
+
         GameObject temp = collision.gameObject;
-        Vector3 p = temp.transform.position;
-        temp.transform.position = new Vector3(Random.Range(-23f,24f), Random.Range(-13f, 17f), 0);
+        SpriteRenderer rasdf = temp.GetComponent<SpriteRenderer>();
+        Color newColor = rasdf.material.color;
+        newColor.a = 0;
+        rasdf.material.color = newColor;
+        
+
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
