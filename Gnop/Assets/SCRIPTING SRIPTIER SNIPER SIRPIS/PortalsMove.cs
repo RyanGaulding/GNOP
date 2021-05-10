aca3f2097a4dc1b49498c4a006c96e1e2bc462ae
parzival-3x <bg193b@gmail.com>
2021-05-10 12:49:52 -0500

    Update PortalsMove.cs

--- Gnop/Assets/SCRIPTING SRIPTIER SNIPER SIRPIS/PortalsMove.cs
+++ Gnop/Assets/SCRIPTING SRIPTIER SNIPER SIRPIS/PortalsMove.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PortalsMove : MonoBehaviour
 {
     public bool goingUp = true;
+    const float top = 23.3f;
+    const float bottom = -24f;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +14,29 @@ public class PortalsMove : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float y = transform.position.y;
+        if (y < top && y > bottom)
+        {
+            if (goingUp)
+            {
+                transform.localPosition += new Vector3(0, 100f * Time.deltaTime, 0);
+            }
+            else
+                transform.localPosition += new Vector3(0, -100f * Time.deltaTime, 0);
+        }
+        if (y == top || y == bottom)
+        {
+            if (goingUp)
+            {
+                transform.Translate(new Vector3(0, -1, 0));
+            }
+            else
+            {
+                transform.Translate(new Vector3(0, 1, 0));
+            }
+            goingUp = !goingUp;
+        }
     }
 }
