e5315ac5cdb8000bdc25c0d2a7c6a35f070d1a23
parzival-3x <bg193b@gmail.com>
2021-05-12 12:30:32 -0500

    added powerup tags

--- Gnop/Assets/SCRIPTING SRIPTIER SNIPER SIRPIS/PowersMove.cs
+++ Gnop/Assets/SCRIPTING SRIPTIER SNIPER SIRPIS/PowersMove.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PowersMove : MonoBehaviour
 {
-
+    GameObject[] powerups;
     // Start is called before the first frame update
     void Start()
     {
-        
+        powerups = GameObject.FindGameObjectsWithTag("powerups");
     }
 
     // Update is called once per frame
