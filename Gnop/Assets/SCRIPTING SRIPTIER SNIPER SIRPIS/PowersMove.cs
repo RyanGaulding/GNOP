c814ca3203382f7db7ef84516270d94d3547544b
parzival-3x <bg193b@gmail.com>
2021-05-13 12:34:04 -0500

    finished making the powerups move

--- Gnop/Assets/SCRIPTING SRIPTIER SNIPER SIRPIS/PowersMove.cs
+++ Gnop/Assets/SCRIPTING SRIPTIER SNIPER SIRPIS/PowersMove.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PowersMove : MonoBehaviour
 {
     private GameObject[] powerups;
+    float oneSecond = 0;
+    int count = 0;
     // Start is called before the first frame update
     void Start()
     {
         powerups = GameObject.FindGameObjectsWithTag("powerup");
-        Debug.Log(powerups.Length);
+        //Debug.Log(powerups.Length);
         foreach(GameObject i in powerups)
         {
             i.SetActive(false);
@@ -21,13 +23,20 @@ public class PowersMove : MonoBehaviour
             int xNew = UnityEngine.Random.Range(-4, 25);
             int yNew = UnityEngine.Random.Range(-24, 24);
             i.transform.position = new Vector3(xNew, yNew, 0);
-            i.SetActive(true);
+            //i.SetActive(true);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (oneSecond >= 1&&count<5)
+        {
+            powerups[count].SetActive(true);
+            oneSecond = 0;
+            count++;
+        }
+        oneSecond += Time.deltaTime;
+        Debug.Log(oneSecond);
     }
 }
